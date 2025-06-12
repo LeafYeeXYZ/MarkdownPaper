f4a0310895d2640b1613bcd24e485efd657e8e85
LeafYeeXYZ <leafyeexyz@outlook.com>
2025-06-12 14:04:16 +0800

    fix: 修复图片嵌入问题

--- lib/main.ts
+++ lib/main.ts
@@ -1,4 +1,6 @@
-import fs from 'node:fs/promises'
+import { readFileSync } from 'node:fs'
+import { readFile, writeFile } from 'node:fs/promises'
+import { dirname, extname, resolve } from 'node:path'
 import { asBlob } from 'html-docx-js-typescript'
 // @ts-ignore
 import katexCss from 'katex/dist/katex.css' with { type: 'text' }
@@ -13,9 +15,10 @@ import { type MarkdownPaperOptions, themes } from './types'
  */
 export async function render(options: MarkdownPaperOptions): Promise<void> {
 	const theme = await themes[options.theme](options)
-	const markdown = await fs.readFile(options.src, { encoding: 'utf-8' })
+	const markdown = await readFile(options.src, { encoding: 'utf-8' })
 	marked.use(markedKatex({ throwOnError: false }))
-	const html = await theme.preParseHtml(`
+	const html = (
+		await theme.preParseHtml(`
     <!DOCTYPE html>
     <html lang="zh-CN">
     <head>
@@ -29,6 +32,17 @@ export async function render(options: MarkdownPaperOptions): Promise<void> {
     </body>
     </html>
   `)
+	).replace(/<img src="(.+?)"/g, (match, p1) => {
+		if (p1.startsWith('http')) return match
+		try {
+			const url = resolve(dirname(options.src), decodeURI(p1))
+			const data = readFileSync(url).toString('base64')
+			return `<img src="data:image/${extname(p1).replace('.', '')};base64,${data}"`
+		} catch (_) {
+			console.error(`图片"${p1}"不存在`)
+			return match
+		}
+	})
 	const browser = await puppeteer.launch()
 	const page = await browser.newPage()
 	await page.setContent(html)
@@ -36,14 +50,14 @@ export async function render(options: MarkdownPaperOptions): Promise<void> {
 	await page.pdf({ path: options.out, ...theme.pdfOptions })
 	const finalHtml = await page.content()
 	if (options.outputHtml) {
-		await fs.writeFile(options.out.replace('.pdf', '.html'), finalHtml)
+		await writeFile(options.out.replace('.pdf', '.html'), finalHtml)
 	}
 	if (options.outputDocx) {
 		const docx = await asBlob(finalHtml)
 		const docxBuffer = new Uint8Array(
 			docx instanceof Blob ? await docx.arrayBuffer() : docx.buffer,
 		)
-		await fs.writeFile(options.out.replace('.pdf', '.docx'), docxBuffer)
+		await writeFile(options.out.replace('.pdf', '.docx'), docxBuffer)
 		console.warn('注意: 导出的Word文件可能存在格式丢失, 请手动调整')
 	}
 	await browser.close()
